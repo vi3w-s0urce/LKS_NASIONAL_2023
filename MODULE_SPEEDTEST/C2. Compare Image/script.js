51df3f4acb38e864bce6698331c0ffab8ff9ade0
xG4L1L30x <macdizofti032@gmail.com>
2023-10-02 00:40:51 +0700

    update c2

--- MODULE_SPEEDTEST/C2. Compare Image/script.js
+++ MODULE_SPEEDTEST/C2. Compare Image/script.js
@@ -1,42 +1,41 @@
-const gray_img = document.getElementById("gray");
-const container = document.getElementById("container");
-const circle = document.getElementById("circle");
-const line = document.getElementById("line");
-const img_width = 600;
+const circle = document.getElementById('circle');
+const line = document.getElementById('line');
+const gray = document.getElementById('gray');
+const width_image = 400;
 
 let mousedown = false;
-let mouse_x;
 let width_percent = 50;
+let mouse_position;
 
+function change() {
+    gray.style.width = width_percent + "%";
+    circle.style.left = width_percent - 2.2 + "%";
+    line.style.left = width_percent - 0.2 + "%"
+}
 
-circle.addEventListener("mousedown", function (e) {
+circle.addEventListener("mousedown", ()=> {
     mousedown = true;
-    console.log("tekan");
+    console.log('tekan');
 })
 
-document.addEventListener("mousemove", function (e) {
+console.log(width_percent);
+
+document.addEventListener("mousemove", (e)=> {
     if (mousedown) {
-        mouse_x = e.clientX - 20;
-        width_percent = (mouse_x / img_width) * 100;
+        mouse_position = e.clientX - 20;
+        width_percent = (mouse_position / width_image) * 100;
+        if (width_percent >= 100) {
+            width_percent = 100;
+        } else if (width_percent <= 0) {
+            width_percent = 0;
+        }
         change();
     }
 })
 
-
-document.addEventListener("mouseup", function (e) {
+document.addEventListener("mouseup", ()=> {
     mousedown = false;
-    console.log("lepas");
+    console.log('lepas');
 })
-
-function change() {
-    if (width_percent <= 0) {
-        width_percent = 0;
-    } else if (width_percent >= 100) {
-        width_percent = 100;
-    }
-    circle.style.left = width_percent - 2.3 + "%";
-    line.style.left = width_percent - 0.3 + "%";
-    gray_img.style.width = width_percent + "%";
-}
 
 change();
